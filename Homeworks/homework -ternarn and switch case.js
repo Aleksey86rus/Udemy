484e25ad182a26c321b66b3c3c31457bf31ed527
Aleksey Bulanov <Aleksey86rus>
2022-08-21 21:10:57 +0500

    swith

---
+++ Homeworks/homework -ternarn and switch case.js
@@ -0,0 +1,42 @@
+1.
+// let a = 'block';
+
+// switch (a) {
+//   case 'block':
+//     console.log('block');
+//     break;
+//   case 'none':
+//     console.log('none');
+//   case 'inline':
+//     console.log('inline');
+//   default:
+//     console.log('other');
+// }
+
+2.
+// let b = 'hidden';
+
+// b = b === 'hidden' ? 'visible' : 'hidden';
+// console.log(b);
+
+// let b = 'hidden';
+// if (b === 'hidden') {
+//   b = 'visible';
+// } else {
+//   b = 'hidden';
+// }
+
+
+3.
+// let c = 0;
+
+// c = c === 0 ? 1 : c < 0 ? 'less then zero' : c *= 10;
+// console.log(c);
+
+// if (c === 0) {
+//   c = 1;
+// } else if (c < 0) {
+//   c = 'less then zero';
+// } else {
+//   c *= 10;
+// }
