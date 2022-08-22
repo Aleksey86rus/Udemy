cddda633f20c7dd29ecde3ca4dc284ebab5ad9d6
Aleksey Bulanov <Aleksey86rus>
2022-08-22 22:25:30 +0500

    added homework-Cycle.js

---
+++ Homeworks/homework-cycle.js
@@ -0,0 +1,73 @@
+1.
+
+//              0 1 23 4 56 7 8910 11 12
+// let userText = 'i   am   in   the     aesycode  sd';
+// let res = '';
+
+
+// for (let i = 0; i < userText.length; i++) {
+//   if (i === 0 || userText[i - 1] === ' ') {
+//     res += userText[i].toUpperCase();
+//   } else {
+//     res += userText[i];
+//   }
+
+// }
+
+// console.log(res);
+
+2.
+// let str = 'tseb eht ma i';
+// let res = '';
+
+// for (let i = str.length; i--;) {
+//   res += str[i];
+// }
+
+// console.log(res);
+
+3.
+// let res = 1;
+
+// for (let i = 1; i <= 10; i++) {
+//   res *= i;
+// }
+// console.log(res);
+
+4.
+// let str = 'JavaScript is a pretty good language';
+// let res = '';
+
+// for (let i = 0; i < str.length; i++) {
+//   if (i === 0 || str[i - 1] === ' ') {
+//     res += str[i].toUpperCase();
+//   } else if (str[i] !== ' ') {
+//     res += str[i];
+//   }
+// }
+
+// console.log(res);
+
+5.
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (let num of arr) {
+//   if (num % 2) {
+//     console.log(num);
+//   }
+// }
+
+6.
+// let list = {
+//   name: 'denis',
+//   work: 'easycode',
+//   age: 29
+// }
+
+// for (let key in list) {
+//   if (typeof list[key] === 'string') {
+//     list[key] = list[key].toUpperCase();
+//   }
+// }
+
+// console.log(list);
